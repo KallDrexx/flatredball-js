851f2e12df8b2686292eddf73833b1290bb97570
KallDrexx <me@mshapiro.net>
2013-03-14 18:53:14 -0400

    Added test assets with starting board

---
+++ main.js
@@ -0,0 +1,12 @@
+﻿var ship = {};
+
+frb.start({
+    init: function () {
+        ship = frb.SpriteManager.add("Ship/f1.png");
+    },
+    
+    update: function () {
+        ship.x = frb.InputManager.mouse.worldX;
+        ship.y = frb.InputManager.mouse.worldY;
+    }
+});
